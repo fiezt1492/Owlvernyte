9677781e9f7ffe37e28e2be05e1cfb5b68d2184d
Fiezt <dat09t@gmail.com>
2022-01-14 17:44:07 +0700

    update blackjack and reduce cooldown to 1

---
+++ commands/gambling/blackjack.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+const blackjack = require("../../modules/gambling/Blackjack/index");
+
+module.exports = {
+	name: "blackjack",
+	description: "Playing Vietnamese Blackjack",
+	category: "gambling",
+	aliases: ["bj"],
+	usage: "",
+	permissions: "SEND_MESSAGES",
+	guildOnly: true,
+
+	async execute(message, args) {
+		const { client } = message;
+
+		await blackjack(client, message);
+	},
+};
