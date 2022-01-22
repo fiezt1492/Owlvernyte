ef0aabe2df240649a5b81adb695bd3129ef649ca
Fiezt <dat09t@gmail.com>
2022-01-22 11:36:10 +0700

    changed

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,7 +1,34 @@
+const { prefix } = require("../config")
+// const guildPrefix = require("../modules/configuration/guildPrefix")
+
 module.exports = {
 	name: "guildCreate",
-    skip: true,
-	async execute(guild,client) {
-        console.log(guild.id)
-    },
+	// skip: true,
+	async execute(guild, client) {
+		// console.log(guild.id);
+        try {
+            const DB = client.db.collection("guildSettings")
+            await DB.updateOne(
+				{
+					gID: guild.id,
+				},
+				{
+					$set: {
+						gID: guild.id,
+						prefix: String(prefix).toLowerCase(),
+					},
+				},
+				{
+					upsert: true,
+				}
+			);
+
+            client.guildSettings.set(guild.id, {
+				prefix: String(prefix).toLowerCase(),
+			});
+        } catch (err) {
+            console.log(err);
+        }
+        
+	},
 };
